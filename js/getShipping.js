27aca1139dc259b2261eb79c2b02b4bbb7cf3d5d
Aaron Lin <phoe721@yahoo.com>
2018-01-19 09:59:59 +0800

    Separate getShipping into by input and by file

--- js/getShipping.js
+++ js/getShipping.js
@@ -3,117 +3,69 @@ jQuery.validator.setDefaults({
     success: "valid"
 });
 
-/* Get Shipping By Input */
-var form2 = $('#form2');
-form2.validate({
+var form = $('#form');
+form.validate({
     rules: {
-        sku: "required",
-        cost: "required",
-        weight: "required",
-        length: "required",
-        width: "required",
-        height: "required"
+        sku: {
+			required: true,
+			number: true
+		},
+        cost: {
+			required: true,
+			number: true
+		},
+        weight: {
+			required: true,
+			number: true
+		},	
+        length: {
+			required: true,
+			number: true
+		},
+        width: {
+			required: true,
+			number: true
+		},
+        height: {
+			required: true,
+			number: true
+		}
     }
-});
-
-$(':text').click(function() { 
-	$(this).val('');
-}); 
-
-$(':text').blur(function() { 
-	if (!$(this).val()) {
-		$(this).val($(this).attr('id'));
-	}
 });
 
 $('#reset').click(function() {
 	$(':text').val('');
+	$('input.error').css({"border-color":"black","border-width":"1px","border-style":"solid"}); 
+	$('#sku-error').html('').removeClass('error valid');
+	$('#cost-error').html('').removeClass('error valid');
+	$('#weight-error').html('').removeClass('error valid');
+	$('#length-error').html('').removeClass('error valid');
+	$('#width-error').html('').removeClass('error valid');
+	$('#height-error').html('').removeClass('error valid');
+	$('#output').html('');
 });
 
 $('#submit').click(function() {
-	var formData = new FormData();
-	formData.append('sku', $('#sku').val());
-	formData.append('cost', $('#cost').val());
-	formData.append('weight', $('#weight').val());
-	formData.append('length', $('#length').val());
-	formData.append('width', $('#width').val());
-	formData.append('height', $('#height').val());
+    if (form.valid()) {
+		var formData = new FormData();
+		formData.append('sku', $('#sku').val());
+		formData.append('cost', $('#cost').val());
+		formData.append('weight', $('#weight').val());
+		formData.append('length', $('#length').val());
+		formData.append('width', $('#width').val());
+		formData.append('height', $('#height').val());
 
-	$.ajax({
-		url: 'script/getShipping.php',
-		data: formData,
-		type: 'POST',
-		contentType: false,
-		processData: false,
-		dataType: 'json',
-		success: function(result) {
-			$('#output').append(result.status + '<br>');
-		}
-	});
-});
-
-/* Get Shipping By File */
-var form = $('#form');
-form.validate({
-    rules: {
-        file: {
-            required: true,
-            accept: 'text/plain',
-            extension: 'txt|csv'
-        }
-    }
-});
-
-$('#file1').change(function() {
-    if (!form.valid()) {
-        console.log('Invalid File!');
-    }
-});
-
-var uid = '';
-$.post('script/getUID.php', {getUID: "yes"}, function(id) {
-	if (typeof id != 'undefined') {
-		uid = id;
-	} else {
-		console.log('Unable to get ID for this process!');
+		$.ajax({
+			url: 'script/getShipping.php',
+			data: formData,
+			type: 'POST',
+			contentType: false,
+			processData: false,
+			dataType: 'json',
+			success: function(result) {
+				$('#output').append(result.status + '<br>');
+			}
+		});
 	}
 });
-
-$('#upload').click(function() {
-	$f1 = $('#file1');
-	var formData = new FormData();
-	formData.append('uid', uid);
-	if($f1.val()) formData.append('file1', $f1.get(0).files[0]);
-
-	$.ajax({
-		url: 'script/getShipping.php',
-		data: formData,
-		type: 'POST',
-		contentType: false,
-		processData: false,
-		dataType: 'json',
-		success: function(result) {
-			$('#output').append(result.status + '<br>');
-		}
-	});
-
-	var time = 10000;
-	var prev = cur = '';
-	var check = setInterval(function(){ 
-		$.post('script/getStatus.php', {uid: uid}, function(result) { 
-			cur = result.status;
-			if (cur.match(/Done/)) {
-				$('#output').html('').append(result.status + "<br>");
-				for (var i = 0; i < result.link.length; i++) {	
-		    		$('#output').append('<a href="' + result.link[i] + '" target="_blank" download>' + result.link[i] + '</a><br>');
-				}
-				clearInterval(check);
-			} else if (prev != cur) {
-				$('#output').append(result.status + "<br>");
-				prev = cur;
-			}
-		}, "json");
-	}, time);
-});
-
 
