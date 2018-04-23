78c54e9d9447ebbb971fb2ef3117629b16d49c27
Aaron Lin <phoe721@yahoo.com>
2018-04-23 11:15:13 +0800

    Change to read file input to remove files.

--- script/ftpDelete.php
+++ script/ftpDelete.php
@@ -5,10 +5,10 @@ require_once('functions.php');
 $server = "www.3pxusa.com";
 $user = "aaron@3pxusa.com";
 $pass = "c7w2l181";
-$folder = "LI";
-$tag = "JPG";
+$folder = "PDEX";
 $files = array();
 $conn = ftp_connect($server) or die("Couldn't connect to $server");
+$input = UPLOAD . "input.txt";
 
 // Login to FTP server
 if (@ftp_login($conn, $user, $pass)) {
@@ -16,20 +16,25 @@ if (@ftp_login($conn, $user, $pass)) {
 	ftp_pasv($conn, true); // Turn Passive Mode On
 	$path = "/images/" . $folder;
 	$files = ftp_nlist($conn, $path);
-	foreach ($files as $file) {
-		if (preg_match('/(\.)' . $tag . '$/', $file)) {
-			echo "Going to remove files with $tag: $file" . PHP_EOL;
-			if (ftp_chdir($conn, $path)) {
-				echo "Changing directory to: " . ftp_pwd($conn) . PHP_EOL;
-				if (ftp_delete($conn, $file)) {
-					echo "Success to remove $file!" . PHP_EOL;
-				} else {
-					echo "Failed to remove $file!" . PHP_EOL;
+	if (ftp_chdir($conn, $path)) {
+		echo "Changing directory to: " . ftp_pwd($conn) . PHP_EOL;
+		$handle = fopen($input, "r");
+		if ($handle) {
+			while (!feof($handle)) {
+				$sku = trim(fgets($handle));
+				$file = $sku . ".jpg";
+				if (in_array($file, $files)) {
+					if (ftp_delete($conn, $file)) {
+						echo "Success to remove $file!" . PHP_EOL;
+					} else {
+						echo "Failed to remove $file!" . PHP_EOL;
+					}
 				}
-			} else {
-				echo "Failed to change directory to $path!" . PHP_EOL;
 			}
 		}
+		fclose($handle);
+	} else {
+		echo "Failed to change directory to $path!" . PHP_EOL;
 	}
 } else {
 	echo "Failed to login to $server!" . PHP_EOL;
