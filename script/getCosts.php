4f578cc3d1d51672ec0fdaa99fa11e3ccfa937f0
Aaron Lin <phoe721@yahoo.com>
2018-05-25 11:16:41 +0800

    Added command line request

--- script/getCosts.php
+++ script/getCosts.php
@@ -59,6 +59,24 @@ if (isset($argv[1]) && isset($argv[2])) {
 
 	log_link_file($result_file);
 	logger("Done!");
+} else if (isset($argv[1])) {
+	$skus = $argv[1];
+	$vendor_code = get_vendor_code($skus);
+	echo "Vendor code: $vendor_code" . PHP_EOL;
+	$item_array = get_set_list($skus);
+	if (empty($item_array)) $item_array = get_item_no($skus);
+	
+	$total_cost = 0;
+	if (check_vendor_code($vendor_code)) {
+		for ($i = 0; $i < count($item_array); $i++) {
+			$cost = get_cost($vendor_code, $item_array[$i]);
+			$total_cost += $cost;
+			echo "Item No.: " . $item_array[$i] . ", Cost: " . $cost . PHP_EOL;
+		}
+		echo "Total Cost: " . $total_cost . PHP_EOL;
+	} else {
+		echo "Invalid Vendor Code!" . PHP_EOL;
+	}
 }
 
 function check_vendor_code($vendor_code) {
--- script/test.php
+++ script/test.php
@@ -1,26 +1,5 @@
 <?	
 /* Initialization */
 require_once("functions.php");
-require_once("getCosts.php");
-
-if (isset($argv[1])) {
-	$skus = $argv[1];
-	$vendor_code = get_vendor_code($skus);
-	echo "Vendor code: $vendor_code" . PHP_EOL;
-	$item_array = get_set_list($skus);
-	if (empty($item_array)) $item_array = get_item_no($skus);
-	
-	$total_cost = 0;
-	if (check_vendor_code($vendor_code)) {
-		for ($i = 0; $i < count($item_array); $i++) {
-			$cost = get_cost($vendor_code, $item_array[$i]);
-			$total_cost += $cost;
-			echo "Item No.: " . $item_array[$i] . ", Cost: " . $cost . PHP_EOL;
-		}
-		echo "Total Cost: " . $total_cost . PHP_EOL;
-	} else {
-		echo "Invalid Vendor Code!" . PHP_EOL;
-	}
-}
 
 ?>
