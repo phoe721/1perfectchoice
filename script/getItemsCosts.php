2b0cd8b5f0aaac147050447ccc83a6df835e46de
Aaron Lin <phoe721@yahoo.com>
2018-05-25 15:58:10 +0800

    Fix logic bug

--- script/getItemsCosts.php
+++ script/getItemsCosts.php
@@ -33,24 +33,22 @@ if (isset($argv[1]) && isset($argv[2])) {
 	$file1 = fopen($input_file, "r");
 	if ($file1) {
 		while (!feof($file1)){
-			$skus = fgets($file1);
-			$skus = trim($skus);
-			$vendor_code = get_vendor_code($skus);
-			$item_array = get_set_list($skus);
-			if (empty($item_array)) $item_array = get_item_no($skus);
-
-			$total_cost = 0;
+			$sku = trim(fgets($file1));
+			$vendor_code = get_vendor_code($sku);
 			if (check_vendor_code($vendor_code)) {
+				$item_array = get_set_list($skus);
+				$total_cost = 0;
 				for ($i = 0; $i < count($item_array); $i++) {
 					$cost = get_cost($vendor_code, $item_array[$i]);
 					$total_cost += $cost;
 				}
 				logger("Total Cost: " . $total_cost);
+
+				$output = $sku . "\t" . $total_cost;
+				log_result($output);
 			} else {
 				logger("Invalid Vendor Code!");
 			}
-			$output = $skus . "\t" . implode("\t", $sku);
-			log_result($output);
 		}
 	} else {
 		logger("Failed to open $input_file");
@@ -60,14 +58,12 @@ if (isset($argv[1]) && isset($argv[2])) {
 	log_link_file($result_file);
 	logger("Done!");
 } else if (isset($argv[1])) {
-	$skus = $argv[1];
-	$vendor_code = get_vendor_code($skus);
+	$sku = $argv[1];
+	$vendor_code = get_vendor_code($sku);
 	echo "Vendor code: $vendor_code" . PHP_EOL;
-	$item_array = get_set_list($skus);
-	if (empty($item_array)) $item_array = get_item_no($skus);
-	
-	$total_cost = 0;
 	if (check_vendor_code($vendor_code)) {
+		$item_array = get_set_list($vendor_code, $sku);
+		$total_cost = 0;
 		for ($i = 0; $i < count($item_array); $i++) {
 			$cost = get_cost($vendor_code, $item_array[$i]);
 			$total_cost += $cost;
@@ -90,94 +86,81 @@ function check_vendor_code($vendor_code) {
 function get_vendor_code($sku) {
 	if (!empty($sku)) {
 		$pieces = explode("-", $sku);
-		return $pieces[0];
+		$vendor_code = $pieces[0];
+
+		return $vendor_code;
 	} else {
 		logger("Invalid SKU: $sku!");
+		return false;
 	}
 }
 
-function get_item_no($sku) {
+function check_item_no($vendor_code, $item) {
 	global $db;
-	$vendor_code = get_vendor_code($sku); //ex. AC-00114, result: AC
-	$item_str = str_replace($vendor_code . "-", "", $sku); //ex. AC-00114, result: 00114
-	$result = $db->query("SELECT item_no FROM costs WHERE vendor_code = '" . $vendor_code . "' AND item_no = '" . $item_str . "'");
-	if (mysqli_num_rows($result) > 0) { // Single item
+	$result = $db->query("SELECT item_no FROM costs WHERE vendor_code = '" . $vendor_code . "' AND item_no = '" . $item . "'");
+	if (mysqli_num_rows($result) > 0) {
 		while ($row = mysqli_fetch_array($result)) {
 			$item_no = $row['item_no'];
 			logger("Found item: " . $item_no);
-			$item_array[0] = $item_no;
+			return true;
 		}
-	} else { // Set items
-		$item_array = explode("-", $item_str); //ex. 00114-15, result: 00114, 15
-		$first_len = strlen($item_array[0]); //ex. 00114, result: 5
-		for ($i = 1; $i < count($item_array); $i++) {
-			$current_len = strlen($item_array[$i]);
-			if ($first_len > $current_len) {
-				$diff = $first_len - $current_len;
-				$item_array[$i] = substr($item_array[0], 0, $diff) . $item_array[$i];
-			}
-		}
+	} else {
+		logger("Item Not Found: $item");
+		return false;
 	}
-
-	return $item_array;
 }
 
-function get_set_list($sku) {
+function get_set_list($vendor_code, $sku) {
 	global $db;
-	$vendor_code = get_vendor_code($sku);
-	$item_str = str_replace($vendor_code . "-", "", $sku);
-
 	$item_array = array();
-	$result = $db->query("SELECT sku1, sku2, sku3, sku4, sku5, sku6, sku7, sku8, sku9, sku10 FROM set_list WHERE vendor_code = '" . $vendor_code . "' AND sku = '" . $item_str . "'");
-	if ($result) { 
-		while ($row = mysqli_fetch_array($result)) {
-			for ($i = 1; $i <= 10; $i++) {
-				$current_sku = $row['sku' . $i];
-				if (!empty($current_sku)) {
-					$item_array[$i-1] = $current_sku;
+	$item_str = str_replace($vendor_code . "-", "", $sku);
+	$item_count = substr_count($item_str, "-") + 1;
+	if ($item_count == 1) {
+		$item_array[0] = $item_str;
+	} else {
+		$result = $db->query("SELECT sku1, sku2, sku3, sku4, sku5, sku6, sku7, sku8, sku9, sku10 FROM set_list WHERE vendor_code = '" . $vendor_code . "' AND sku = '" . $item_str . "'");
+		if ($result) { 
+			while ($row = mysqli_fetch_array($result)) {
+				for ($i = 1; $i <= 10; $i++) {
+					$current_sku = $row['sku' . $i];
+					if (!empty($current_sku)) {
+						$item_array[$i-1] = $current_sku;
+					}
+				}
+			}
+		} else {
+			$item_array = explode("-", $item_str); //ex. 00114-15, result: 00114, 15
+			$first_len = strlen($item_array[0]); //ex. 00114, result: 5
+			for ($i = 0; $i < count($item_array); $i++) {
+				$current_len = strlen($item_array[$i]);
+				if ($first_len > $current_len) {
+					$diff = $first_len - $current_len;
+					$item_array[$i] = substr($item_array[0], 0, $diff) . $item_array[$i];
 				}
 			}
 		}
-	} else {
-		logger("Failed to request database!");
+	}
+
+	for ($i = 0; $i < count($item_array); $i++) {
+		check_item_no($vendor_code, $item_array[$i]);
 	}
 
 	return $item_array;
 }
 
-function get_cost_by_sku($sku) {
-	$vendor_code = get_vendor_code($skus);
-	logger("Vendor code: $vendor_code");
-	$item_array = get_set_list($skus);
-	if (empty($item_array)) $item_array = get_item_no($skus);
-	
-	$total_cost = 0;
-	if (check_vendor_code($vendor_code)) {
-		for ($i = 0; $i < count($item_array); $i++) {
-			$cost = get_cost($vendor_code, $item_array[$i]);
-			$total_cost += $cost;
-			logger("Item No.: " . $item_array[$i] . ", Cost: " . $cost);
-		}
-		logger("Total Cost: " . $total_cost);
-	} else {
-		logger("Invalid Vendor Code!");
-	}
-
-	return $total_cost;
-} 
-
 function get_cost($vendor_code, $item_no) {
 	global $db;
+	$cost = -1;
 	$result = $db->query("SELECT cost FROM costs WHERE vendor_code = '" . $vendor_code . "' AND item_no = '" . $item_no . "'"); 
 	if ($result) {
 		while ($row = mysqli_fetch_array($result)) {
 			$cost = $row['cost'];
 			logger("Found item " . $item_no . ": $cost");
-
-			return $cost;
 		}
 	} else {
 		logger("Cost not found!");
 	}
+
+	return $cost;
 }
 ?>
