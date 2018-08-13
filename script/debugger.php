7ed959fca55d8a051c1b747c9deec61d4f4e725c
Aaron Lin <phoe721@gmail.com>
2018-08-13 19:28:12 +0800

    Added different levels of messages

--- script/debugger.php
+++ script/debugger.php
@@ -9,10 +9,30 @@ class debugger {
 		} 
 	}
 
+	public function notice($message) {
+		if ($this->debug) {
+			echo "[Notice] $message\n";
+		} 
+	}
+
+	public function warning($message) {
+		if ($this->debug) {
+			echo "[Warning] $message\n";
+		}
+	}
+
 	public function error($message) {
 		if ($this->debug) {
 			echo "[Error] $message\n";
 		}
+	}
+
+	public function debug_on() {
+		$this->debug = 1;
+	}
+
+	public function debug_off() {
+		$this->debug = 0;
 	}
 }
 ?>
