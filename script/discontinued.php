4cca7817fa2ad24f6e579a23c49fa8f332561978
Aaron Lin <phoe721@gmail.com>
2018-08-13 20:16:24 +0800

    Updates

--- script/discontinued.php
+++ script/discontinued.php
@@ -9,35 +9,36 @@ class discontinued {
 
 	public function __construct() {
 		$this->output = new debugger;
+		$this->output->debug_on();
 		$this->db = new database;
 		$this->db->connect(DB_SERVER, DB_USER, DB_PASS, DATABASE);
 		mysqli_set_charset($this->db->getConnection(), "utf8");
 	}
 
-	public function insert_discontinued_product($code, $item_no) {
+	public function insert_discontinued($code, $item_no) {
 		$result = $this->db->query("INSERT INTO discontinued (did, code, item_no) VALUES ('', '$code', '$item_no')");
 		if ($result) {
-			$this->output->info("Item: $item_no, Code: $code has been inserted to discontinued table successfully!");
+			$this->output->info("Item: $item_no, Code: $code has been inserted successfully!");
 		} else {
-			$this->output->error("Failed to insert $item_no to discontinued table!");
+			$this->output->error("Failed to insert $item_no!");
 		}
 	}
 	
 	public function truncate_discontinued_table() {
 		$result = $this->db->query("TRUNCATE TABLE discontinued");
 		if ($result) {
-			$this->output->info("Discontinued table truncated!");
+			$this->output->info("Table truncated!");
 		} else {
-			$this->output->error("Failed to truncate discontinued table!");
+			$this->output->error("Failed to truncate!");
 		}
 	}
 	
 	public function update_discontinued_table_by_file($filePath) {
 		$result = $this->db->query("LOAD DATA LOCAL INFILE '$filePath' INTO TABLE discontinued");
 		if ($result) {
-			$this->output->info("Discontinued table updated with $filePath!");
+			$this->output->info("Table updated with $filePath!");
 		} else {
-			$this->output->error("Failed to update discontinued table with $filePath!");
+			$this->output->error("Failed to update table with $filePath!");
 		}
 	}
 	
