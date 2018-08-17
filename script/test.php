f3c3ab186a6e6d51d1a48ca5c1aec967198f39b4
Aaron Lin <phoe721@gmail.com>
2018-08-17 13:59:16 +0800

    Test discontinued

--- script/test.php
+++ script/test.php
@@ -2,6 +2,17 @@
 /* Initialization */
 require_once("discontinued.php");
 $dis = new discontinued();
-$dis->check('FA','CM-AC135WH');
 
+$handle = fopen(UPLOAD . "check.txt", "r");
+while (!feof($handle)) {
+	$line = trim(fgets($handle));
+	if (!empty($line)) {
+		$pos = strpos($line, "-");
+		$code = substr($line, 0, $pos);
+		$item_no = substr($line, $pos + 1);
+		//echo $code . ", " . $item_no . PHP_EOL;
+		$dis->check($code, $item_no);
+	}
+}
+fclose($handle);
 ?>
