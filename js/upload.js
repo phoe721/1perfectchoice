2eeac4373cbc5d66aebd1bd8700142ad92174e3c
Aaron Lin <phoe721@gmail.com>
2018-10-02 14:00:37 +0800

    Removed getCategory

--- js/upload.js
+++ js/upload.js
@@ -26,3 +26,42 @@ $('#file').change(function() {
         console.log('Invalid File!');
     }
 });
+
+/*
+$('#upload').click(function() {
+	$f1 = $('#file');
+	var formData = new FormData();
+	formData.append('uid', uid);
+	if($f1.val()) formData.append('file', $f1.get(0).files[0]);
+
+	$.ajax({
+		url: 'script/getCategory.php',
+		data: formData,
+		type: 'POST',
+		contentType: false,
+		processData: false,
+		dataType: 'json',
+		success: function(result) {
+			$('#output').append(result.status + '<br>');
+		}
+	});
+
+	var time = 10000;
+	var prev = cur = '';
+	var check = setInterval(function(){ 
+		$.post('script/getStatus.php', {uid: uid}, function(result) { 
+			cur = result.status;
+			if (cur.match(/Done/)) {
+				$('#output').html('').append(result.status + "<br>");
+				for (var i = 0; i < result.link.length; i++) {	
+		    		$('#output').append('<a href="' + result.link[i] + '" target="_blank" download>' + result.link[i] + '</a><br>');
+				}
+				clearInterval(check);
+			} else if (prev != cur) {
+				$('#output').append(result.status + "<br>");
+				prev = cur;
+			}
+		}, "json");
+	}, time);
+});
+*/
