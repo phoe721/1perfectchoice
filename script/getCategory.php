ca47f63d85f0d0cd43611f0b72d1fa133dcd79c0
Aaron Lin <phoe721@gmail.com>
2018-10-02 11:44:57 +0800

    Fix output bug

--- script/getCategory.php
+++ script/getCategory.php
@@ -2,22 +2,25 @@
 require_once('class/category.php');
 $cat = new category();
 
-if (isset($argv[1]) && isset($argv[2]) && isset($argv[3])) {
-	$uid = $argv[1];
-	$input = $argv[2];
-	$output = $argv[3];
-	$file = fopen($input, "r");
-	$file2 = fopen($output, "a+");
+if (isset($argv[1]) && isset($argv[2])) {
+	$inputFile = $argv[1];
+	$outputDir = $argv[2];
+	$outputFile = $outputDir . "/result.txt";
+	$file = fopen($inputFile, "r");
+	$file2 = fopen($outputFile, "a+");
 	if ($file && $file2) {
 		while(!feof($file)) {
 			$title = trim(fgets($file));
-			if (!empty($line)) {
-				$output = $title . "\t" . get_category($title) . PHP_EOL;
+			if (!empty($title)) {
+				$output = $title . "\t" . $cat->get_category($title) . PHP_EOL;
 				fwrite($file2, $output);
 			}
 		}
 	}
 	fclose($file);
 	fclose($file2);
+} else if (isset($argv[1])) {
+	$input = $argv[1];
+	echo $cat->get_category($input);
 }
 ?>
