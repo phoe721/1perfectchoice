6e42875d47db07cb0d9900bf440a534f3184c865
Aaron Lin <phoe721@gmail.com>
2018-10-02 18:42:42 +0800

    Update

--- script/upload.php
+++ script/upload.php
@@ -4,11 +4,11 @@ require_once('class/upload.php');
 require_once('class/queues.php');
 $upload = new upload();
 $queue = new queues();
-$script = "";
 
 if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['file']) && isset($_POST['task']) && isset($_POST['uid'])) { 
 	$upload->set_UID($_POST['uid']);
 	$upload->set_file($_FILES['file']);
+	$script = "";
 	$output = $upload->get_error();
 	if ($output == "File uploaded!") {
 		$task = $_POST['task'];
