081047ba7a74e50d05b726d2fa577fe28ea390c1
Aaron Lin <phoe721@gmail.com>
2018-10-03 21:23:52 +0800

    Reduce loglevel

--- script/class/debugger.php
+++ script/class/debugger.php
@@ -7,23 +7,31 @@ class debugger {
 	private $statusFile;
 
 	public function info($message) {
-		if ($this->loglevel == 0) echo $message . PHP_EOL;
-		$this->logger("[Info] $message");
+		if ($this->loglevel == 0) {
+			echo $message . PHP_EOL;
+			$this->logger("[Info] $message");
+		}
 	}
 
 	public function notice($message) {
-		if ($this->loglevel >= 1) echo $message . PHP_EOL;
-		$this->logger("[Notice] $message");
+		if ($this->loglevel >= 1) {
+			echo $message . PHP_EOL;
+			$this->logger("[Notice] $message");
+		}
 	}
 
 	public function warning($message) {
-		if ($this->loglevel >= 2) echo $message . PHP_EOL;
-		$this->logger("[Warning] $message");
+		if ($this->loglevel >= 2) {
+			echo $message . PHP_EOL;
+			$this->logger("[Warning] $message");
+		}
 	}
 
 	public function error($message) {
-		if ($this->loglevel >= 3) echo $message . PHP_EOL;
-		$this->logger("[Error] $message");
+		if ($this->loglevel >= 3) {
+			echo $message . PHP_EOL;
+			$this->logger("[Error] $message");
+		}
 	}
 
 	public function set_log_level($level) {
