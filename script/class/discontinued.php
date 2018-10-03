c5387ecf34941ca032fe01e5c2bcb223eba78272
Aaron Lin <phoe721@gmail.com>
2018-10-03 08:34:52 +0800

    Corrected discontinued class output

--- script/class/discontinued.php
+++ script/class/discontinued.php
@@ -17,8 +17,10 @@ class discontinued {
 		$result = $this->db->query("INSERT INTO discontinued (code, item_no) VALUES ('$code', '$item_no')");
 		if ($result) {
 			$this->output->info("Item: $item_no, Code: $code has been inserted successfully!");
+			return true;
 		} else {
 			$this->output->error("Failed to insert $item_no!");
+			return false;
 		}
 	}
 	
@@ -26,8 +28,10 @@ class discontinued {
 		$result = $this->db->query("SELECT * FROM discontinued WHERE code = '$code' AND item_no = '$item_no'");
 		if (mysqli_num_rows($result) > 0) {
 			$this->output->info("Item: $item_no, Code: $code is discontinued!");
+			return true;
 		} else {
 			$this->output->info("Item: $item_no, Code: $code is still active!");
+			return false;
 		}
 	}
 
@@ -35,8 +39,10 @@ class discontinued {
 		$result = $this->db->query("TRUNCATE TABLE discontinued");
 		if ($result) {
 			$this->output->info("Table truncated!");
+			return true;
 		} else {
 			$this->output->error("Failed to truncate!");
+			return false;
 		}
 	}
 	
@@ -44,12 +50,15 @@ class discontinued {
 		$result = $this->db->query("LOAD DATA LOCAL INFILE '$filePath' INTO TABLE discontinued");
 		if ($result) {
 			$this->output->info("Table updated with $filePath!");
+			return true;
 		} else {
 			$this->output->error("Failed to update table with $filePath!");
+			return false;
 		}
 	}
 	
 	public function get_record_count() {
+		$count = -1;
 		$result = $this->db->query("SELECT * FROM discontinued");
 		if ($result) {
 			$count = mysqli_num_rows($result);
@@ -57,6 +66,8 @@ class discontinued {
 		} else {
 			$this->output->error("Failed to get record count in table discontinued!");
 		}
+
+		return $count;
 	}
 }
 
