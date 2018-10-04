600c14319ec6bfebf676bf4270720e718ec3162e
Aaron Lin <phoe721@gmail.com>
2018-10-04 23:35:40 +0800

    Added checkInventory

---
+++ script/checkInventory.php
@@ -0,0 +1,41 @@
+<?
+require_once("class/inventory.php");
+$inventory = new inventory();
+$status = new debugger();
+
+if (isset($argv[1]) && isset($argv[2]) && isset($argv[3])) {
+	$inputFile = $argv[1];
+	$outputFile = $argv[2];
+	$statusFile = $argv[3];
+	$status->set_status_file($statusFile);
+	$input = fopen($inputFile, "r");
+	$output = fopen($outputFile, "a+");
+	if ($input && $output) {
+		while(!feof($input)) {
+			$sku = trim(fgets($input));
+			if (!empty($sku)) {
+				$status->log_status("Checking $sku...");
+				if (preg_match('/^[A-Z]+-[A-Z0-9-]+$/', $sku)) {
+					list($code, $item_no) = explode("-", $sku, 2);
+					$qty = $inventory->get_inventory($code, $item_no);
+					$result = "$sku\t$qty" . PHP_EOL;
+				} else {
+					$status->info("Invalid SKU: $sku");
+					$result = "$sku\t-" . PHP_EOL;
+				}
+				fwrite($output, $result);
+			}
+		}
+	}
+
+	$status->log_status("Done!");
+	fclose($input);
+	fclose($output);
+}
+
+if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["sku"])) { 
+	$sku = $_POST["sku"];
+	list($code, $item_no) = explode("-", $sku, 2);
+	$inventory->get_inventory($code, $item_no);
+}
+?>
