c0af5d219e25b2038277455f90dd92110aa687b8
Aaron Lin <phoe721@gmail.com>
2018-10-10 10:12:09 +0800

    Fix discontinued: check set list Added getCategory selection to menu

--- script/class/discontinued.php
+++ script/class/discontinued.php
@@ -1,16 +1,19 @@
 <?
 /* Initialization */
 require_once("database.php");
+require_once("set_list.php");
 
 class discontinued {
 	private $db;
 	private $output;
+	private $set_list;
 
 	public function __construct() {
 		$this->output = new debugger;
 		$this->db = new database;
 		$this->db->connect(DB_SERVER, DB_USER, DB_PASS, DATABASE);
 		mysqli_set_charset($this->db->getConnection(), "utf8");
+		$this->set_list = new set_list();
 	}
 
 	public function insert($code, $item_no) {
@@ -36,13 +39,36 @@ class discontinued {
 	}
 
 	public function check($code, $item_no) {
-		$result = $this->db->query("SELECT * FROM discontinued WHERE code = '$code' AND item_no = '$item_no'");
-		if (mysqli_num_rows($result) > 0) {
-			$this->output->notice("Item: $item_no, Code: $code is discontinued!");
-			return true;
+		if ($this->set_list->check($code, $item_no)) {
+			$count = 0;
+			$set = $this->set_list->get_set($code, $item_no);
+			for ($i = 0; $i < count($set); $i++) {
+				$item = $set[$i];
+				$result = $this->db->query("SELECT * FROM discontinued WHERE code = '$code' AND item_no = '$item'");
+				if (mysqli_num_rows($result) > 0) {
+					$this->output->notice("Item: $item, Code: $code is discontinued!");
+					$count++;
+				} else {
+					$this->output->notice("Item: $item, Code: $code is still active!");
+				}
+			}
+
+			if ($count > 0) {
+				$this->output->notice("Item: $item_no, Code: $code is discontinued!");
+				return true;
+			} else {
+				$this->output->notice("Item: $item_no, Code: $code is still active!");
+				return false;
+			}
 		} else {
-			$this->output->notice("Item: $item_no, Code: $code is still active!");
-			return false;
+			$result = $this->db->query("SELECT * FROM discontinued WHERE code = '$code' AND item_no = '$item_no'");
+			if (mysqli_num_rows($result) > 0) {
+				$this->output->notice("Item: $item_no, Code: $code is discontinued!");
+				return true;
+			} else {
+				$this->output->notice("Item: $item_no, Code: $code is still active!");
+				return false;
+			}
 		}
 	}
 
--- script/getCategory.php
+++ script/getCategory.php
@@ -1,26 +1,37 @@
 <?
 require_once('class/category.php');
 $cat = new category();
+$status = new debugger();
 
-if (isset($argv[1]) && isset($argv[2])) {
+if (isset($argv[1]) && isset($argv[2]) && isset($argv[3])) {
 	$inputFile = $argv[1];
-	$outputDir = $argv[2];
-	$outputFile = $outputDir . "/result.txt";
-	$file = fopen($inputFile, "r");
-	$file2 = fopen($outputFile, "a+");
-	if ($file && $file2) {
-		while(!feof($file)) {
-			$title = trim(fgets($file));
+	$outputFile = $argv[2];
+	$statusFile = $argv[3];
+	$status->set_status_file($statusFile);
+	$input = fopen($inputFile, "r");
+	$output = fopen($outputFile, "a+");
+	if ($input && $output) {
+		while(!feof($input)) {
+			$title = trim(fgets($input));
 			if (!empty($title)) {
-				$output = $title . "\t" . $cat->get_category($title) . PHP_EOL;
-				fwrite($file2, $output);
+				$status->log_status("Checking $title...");
+				$category = $cat->get_category($title);
+				$result = "$title\t$category" . PHP_EOL;
+				fwrite($output, $result);
 			}
 		}
 	}
-	fclose($file);
-	fclose($file2);
-} else if (isset($argv[1])) {
-	$input = $argv[1];
-	echo $cat->get_category($input);
+
+	$status->log_status("Done!");
+	fclose($input);
+	fclose($output);
+}
+
+if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["sku"])) { 
+	$title = $_POST["sku"];
+	$category = $cat->get_category($title);
+	$result = "$title category: $category!";
+
+	echo json_encode($result);
 }
 ?>
