1c1b2d407fa1cb68c47b6d0738e4b30f4cb2842b
Aaron Lin <phoe721@gmail.com>
2018-10-12 11:51:10 +0800

    Fix checkLinks: accept input parameters.

--- script/checkLinks.php
+++ script/checkLinks.php
@@ -1,6 +1,32 @@
 <?
 require_once("class/check_links.php");
 $cl = new check_links();
-$cl->check_links_by_file(UPLOAD . "input.txt");
+$status = new debugger();
 
+if (isset($argv[1]) && isset($argv[2]) && isset($argv[3])) {
+	$inputFile = $argv[1];
+	$outputFile = $argv[2];
+	$statusFile = $argv[3];
+	$status->set_status_file($statusFile);
+	$input = fopen($inputFile, "r");
+	$output = fopen($outputFile, "a+");
+	if ($input && $output) {
+		while(!feof($input)) {
+			$url = trim(fgets($input));
+			if (!empty($url)) {
+				$status->log_status("Checking $url...");
+				if ($cl->check_link($url)) {
+					$result = "$url\tOK" . PHP_EOL;
+				} else {
+					$result = "$url\tFail" . PHP_EOL;
+				}
+				fwrite($output, $result);
+			}
+		}
+	}
+
+	$status->log_status("Done!");
+	fclose($input);
+	fclose($output);
+}
 ?>
