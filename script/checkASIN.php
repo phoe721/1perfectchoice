dd01f2b4eebaa55b2fec309f32ab3638d4141d45
Aaron Lin <phoe721@gmail.com>
2018-10-29 10:08:16 +0800

    Added checkASIN script

---
+++ script/checkASIN.php
@@ -0,0 +1,46 @@
+<?
+require_once("class/ASIN.php");
+require_once("class/status.php");
+require_once("class/validator.php");
+$a = new ASIN();
+$status = new status();
+$validator = new validator();
+
+if (isset($argv[1]) && isset($argv[2]) && isset($argv[3])) {
+	$inputFile = $argv[1];
+	$outputFile = $argv[2];
+	$statusFile = $argv[3];
+	$status->set_file($statusFile);
+	$input = fopen($inputFile, "r");
+	$output = fopen($outputFile, "a+");
+	if ($input && $output) {
+		while(!feof($input)) {
+			$sku = trim(fgets($input));
+			if (!empty($sku)) {
+				$status->log_status("Checking $sku...");
+				if ($validator->check_sku($sku)) {
+					list($code, $item_no) = explode("-", $sku, 2);
+					$asin = $a->get_asin($code, $item_no);
+					$result = "$sku\t$asin" . PHP_EOL;
+				} else {
+					$result = "$sku\tInvalid" . PHP_EOL;
+				}
+				fwrite($output, $result);
+			}
+		}
+	}
+
+	$status->log_status("Done!");
+	fclose($input);
+	fclose($output);
+}
+
+if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["sku"])) { 
+	$sku = $_POST["sku"];
+	list($code, $item_no) = explode("-", $sku, 2);
+	$asin = $a->get_asin($code, $item_no);
+	$result = "$sku has ASIN $asin!";
+
+	echo json_encode($result);
+}
+?>
---
+++ script/class/ASIN.php
@@ -0,0 +1,76 @@
+<?
+/* Initialization */
+require_once("database.php");
+
+class ASIN {
+	private $db;
+	private $output;
+
+	public function __construct() {
+		$this->output = new debugger;
+		$this->db = new database;
+		$this->db->connect(DB_SERVER, DB_USER, DB_PASS, DATABASE);
+		mysqli_set_charset($this->db->getConnection(), "utf8");
+	}
+
+	public function insert($code, $item_no, $asin) {
+		$result = $this->db->query("INSERT INTO ASIN (code, item_no, asin) VALUES ('$code', '$item_no', '$asin')");
+		if ($result) {
+			$this->output->notice("Item: $item_no, Code: $code with $asin has been inserted successfully!");
+			return true;
+		} else {
+			$this->output->notice("Failed to insert $item_no!");
+			return false;
+		}
+	}
+	
+	public function update($code, $item_no, $asin) {
+		$result = $this->db->query("UPDATE ASIN SET asin = '$asin' WHERE code = '$code' AND item_no = '$item_no'");
+		if ($result) {
+			$this->output->notice("Item: $item_no, Code: $code has updated ASIN to $asin!");
+			return true;
+		} else {
+			$this->output->notice("Item: $item_no, Code: $code failed to update ASIN!");
+			return false;
+		}
+	}
+
+	public function delete($code, $item_no) {
+		$result = $this->db->query("DELETE FROM ASIN WHERE code = '$code' AND item_no = '$item_no'");
+		if ($result) {
+			$this->output->notice("Item: $item_no, Code: $code has been deleted!");
+			return true;
+		} else {
+			$this->output->notice("Item: $item_no, Code: $code failed to delete!");
+			return false;
+		}
+	}
+
+	public function get_asin($code, $item) {
+		$result = $this->db->query("SELECT asin FROM ASIN WHERE code = '$code' AND item_no = '$item'");
+		if (mysqli_num_rows($result) > 0) {
+			$row = mysqli_fetch_array($result);
+			$asin = $row['asin'];
+			$this->output->notice("Item: $item, code: $code ASIN $asin!");
+		} else {
+			$this->output->notice("Item: $item, code: $code ASIN not found!");
+		}
+
+		return $asin;
+	}
+
+	public function get_record_count() {
+		$count = -1;
+		$result = $this->db->query("SELECT * FROM ASIN");
+		if ($result) {
+			$count = mysqli_num_rows($result);
+			$this->output->notice("There are $count records in table ASIN!");
+		} else {
+			$this->output->error("Failed to get record count in table ASIN!");
+		}
+
+		return $count;
+	}
+}
+
+?>
