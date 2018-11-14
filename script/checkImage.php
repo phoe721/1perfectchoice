0fe3525372e37845c1e688dfc4fb63f2cf39e202
Aaron Lin <phoe721@gmail.com>
2018-11-14 08:56:30 +0800

    Fix bug: using validator class

--- script/checkImage.php
+++ script/checkImage.php
@@ -1,13 +1,13 @@
 <?
 require_once("init.php");
-require_once("class/check_links.php");
-$cl = new check_links();
+require_once("class/validator.php");
+$validator = new validator();
 
 if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["sku"])) { 
 	$sku = $_POST["sku"];
 	list($code, $item_no) = explode("-", $sku, 2);
 	$url = IMAGE_SERVER . "$code/$item_no.jpg";
-	if ($cl->check_link($url)) {
+	if ($validator->check_url($url)) {
 		$result = "<img src='$url' width='500px' alt='$sku'>";
 	} else {
 		$result = "<img src='' alt='Not Found'>";
