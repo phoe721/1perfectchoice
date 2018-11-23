1c21efdd811cee0270b82aac5651dbd121f26312
Aaron Lin <phoe721@gmail.com>
2018-11-23 14:34:09 +0800

    Update: remove status function

--- script/class/debugger.php
+++ script/class/debugger.php
@@ -4,7 +4,6 @@ require_once(__DIR__ . "/../init.php");
 
 class debugger {
 	private $loglevel = 0;
-	private $statusFile;
 
 	public function info($message) {
 		if ($this->loglevel == 0) echo $message . PHP_EOL;
@@ -12,36 +11,25 @@ class debugger {
 	}
 
 	public function notice($message) {
-		$this->logger("[Notice] $message");
+		if ($this->loglevel == 1) $this->logger("[Notice] $message");
 	}
 
 	public function warning($message) {
-		$this->logger("[Warning] $message");
+		if ($this->loglevel == 2) $this->logger("[Warning] $message");
 	}
 
 	public function error($message) {
-		$this->logger("[Error] $message");
+		if ($this->loglevel == 3) $this->logger("[Error] $message");
 	}
 
 	public function set_log_level($level) {
 		$this->loglevel = $level;
 	}
 
-	public function set_status_file($path) {
-		$this->statusFile = $path;
-	}
-
 	public function logger($msg) {
 		$timestring = date('Y-m-d H:i:s', strtotime('now'));
 		$msg = "$timestring $msg" . PHP_EOL;
 		$file = fopen(LOG_FILE, 'a+');
-		if ($file) fwrite($file, $msg);
-		fclose($file);
-	}
-
-	public function log_status($msg) {
-		$msg .= PHP_EOL;
-		$file = fopen($this->statusFile, "w");
 		if ($file) fwrite($file, $msg);
 		fclose($file);
 	}
