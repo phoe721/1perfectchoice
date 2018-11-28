8c530438ae06d63c766feaa542047c995a2a39c8
Aaron Lin <phoe721@gmail.com>
2018-11-28 22:40:49 +0800

    Update

--- script/class/costs.php
+++ script/class/costs.php
@@ -49,70 +49,75 @@ class costs {
 		}
 	}
 
-	public function get_cost($code, $item) {
+	public function get_cost($code, $item_no) {
 		$cost = 0;
-		if ($this->set_list->check($code, $item)) {
-			// Set cost
-			$total = 0;
-			$set = $this->set_list->get_set($code, $item);
+		$result = $this->db->query("SELECT cost FROM costs WHERE code = '$code' AND item_no = '$item_no'");
+		if (mysqli_num_rows($result) > 0) {
+			$row = mysqli_fetch_array($result);
+			$cost = $row['cost'];
+			$this->output->notice("Item: $item_no, code: $code costs $cost!");
+		} else {
+			$this->output->notice("Item: $item_no, code: $code cost not found!");
+		}
+
+		if ($this->set_list->check($code, $item_no)) {
+			$costs = array();
+			$set = $this->set_list->get_set($code, $item_no);
 			for ($i = 0; $i < count($set); $i++) {
-				$item_no = $set[$i];
-				$result = $this->db->query("SELECT cost FROM costs WHERE code = '$code' AND item_no = '$item_no'");
+				$item = $set[$i];
+				$result = $this->db->query("SELECT cost FROM costs WHERE code = '$code' AND item_no = '$item'");
 				if (mysqli_num_rows($result) > 0) {
 					$row = mysqli_fetch_array($result);
 					$cost = $row['cost'];
-					$total += $cost;
+					array_push($costs, $cost);
 					$this->output->notice("Item: $item_no, code: $code costs $cost!");
 				} else {
 					$this->output->notice("Item: $item_no, code: $code cost not found!");
 				}
 			}
 
-			// Item cost
-			$result = $this->db->query("SELECT cost FROM costs WHERE code = '$code' AND item_no = '$item'");
-			if (mysqli_num_rows($result) > 0) {
-				$row = mysqli_fetch_array($result);
-				$cost = $row['cost'];
-				$this->output->notice("Item: $item, code: $code costs $cost!");
-			} else {
-				$this->output->notice("Item: $item, code: $code cost not found!");
-			}
 
-			// Get max between item cost & set cost
-			$total = max($cost, $total);
+			// Get MAX(Item Cost, Set Cost)
+			$total = max($cost, array_sum($costs));
 			$this->output->notice("Item: $item, code: $code has total costs $total!");
 			return $total;
 		} else {
-			$result = $this->db->query("SELECT cost FROM costs WHERE code = '$code' AND item_no = '$item'");
-			if (mysqli_num_rows($result) > 0) {
-				$row = mysqli_fetch_array($result);
-				$cost = $row['cost'];
-				$this->output->notice("Item: $item, code: $code costs $cost!");
-			} else {
-				$this->output->notice("Item: $item, code: $code cost not found!");
-			}
-
 			return $cost;
 		}
 	}
 
-	public function get_unit($code, $item) {
+	public function get_unit($code, $item_no) {
 		$unit = 0;
-		if ($this->set_list->check($code, $item)) {
-			$unit = 1; // It's a set!
-			$this->output->notice("Item: $item, Code: $code is a set, unit is $unit!");
+		if ($this->set_list->check($code, $item_no)) {
+			$units = array();
+			$set = $this->set_list->get_set($code, $item_no);
+			for ($i = 0; $i < count($set); $i++) {
+				$item = $set[$i];
+				$result = $this->db->query("SELECT unit FROM costs WHERE code = '$code' AND item_no = '$item'");
+				if (mysqli_num_rows($result) > 0) {
+					$row = mysqli_fetch_array($result);
+					$unit = $row['unit'];
+					array_push($units, $unit);
+					$this->output->notice("Item: $item, Code: $code has $unit unit per box!");
+				} else {
+					$this->output->notice("Item: $item, Code: $code unit per box not found!");
+				}
+			}
+			$this->output->notice("Item: $item, Code: $code is a set!");
+
+			return 1;
 		} else {
-			$result = $this->db->query("SELECT unit FROM costs WHERE code = '$code' AND item_no = '$item'");
+			$result = $this->db->query("SELECT unit FROM costs WHERE code = '$code' AND item_no = '$item_no'");
 			if (mysqli_num_rows($result) > 0) {
 				$row = mysqli_fetch_array($result);
 				$unit = $row['unit'];
-				$this->output->notice("Item: $item, Code: $code has $unit unit per box!");
+				$this->output->notice("Item: $item_no, Code: $code has $unit unit per box!");
 			} else {
-				$this->output->notice("Item: $item, Code: $code unit per box not found!");
+				$this->output->notice("Item: $item_no, Code: $code unit per box not found!");
 			}
-		}
 
-		return $unit;
+			return $unit;
+		}
 	}
 
 	public function get_record_count() {
