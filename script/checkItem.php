d331d2f67e3c57f82ba368fedaf0539406740505
Aaron Lin <phoe721@gmail.com>
2019-01-31 11:06:18 +0800

    Added total weight

--- script/checkItem.php
+++ script/checkItem.php
@@ -78,14 +78,17 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["sku"])) {
 	$box_count = $pg->get_box_count($code, $item_no); 
 	$pg_weights = $pg->get_weight($code, $item_no);
 	$pg_dimensions = $pg->get_dimensions($code, $item_no);
+	$total_weight = 0;
 
 	$result .= "Box Count: $box_count<br>";
 	if (!empty($pg_weights) && !empty($pg_dimensions)) {
 		for ($i = 0; $i < $box_count; $i++) {
 			$count = $i + 1;
+			$total_weight += $pg_weights[$i];
 			$result .= "Box $count Weight: " . $pg_weights[$i] . " lbs<br>";
 			$result .= "Box $count Dimensions: " . $pg_dimensions[$i*3] . " x " . $pg_dimensions[$i*3+1] . " x " . $pg_dimensions[$i*3+2] . "<br>";
 		}
+		$result .= "Total Weight: $total_weight lbs<br>";
 	}
 
 	$result .= "</div>";
