d670836be584a2258483fc14dc005ffb22d844ff
Aaron Lin <phoe721@gmail.com>
2020-03-26 14:08:59 +0800

    Update

---
+++ script/checkTitle.php
@@ -0,0 +1,37 @@
+<?
+require_once("class/product.php");
+require_once("class/status.php");
+require_once("class/validator.php");
+$product = new product();
+$status = new status();
+$validator = new validator();
+
+if (isset($argv[1]) && isset($argv[2]) && isset($argv[3])) {
+	$inputFile = $argv[1];
+	$outputFile = $argv[2];
+	$statusFile = $argv[3];
+	$status->set_file($statusFile);
+	$input = fopen($inputFile, "r");
+	$output = fopen($outputFile, "a+");
+	if ($input && $output) {
+		while(!feof($input)) {
+			$sku = trim(fgets($input));
+			if (!empty($sku)) {
+				$status->log_status("Checking $sku...");
+				if ($validator->check_sku($sku)) {
+					list($code, $item_no) = explode("-", $sku, 2);
+					$title = $product->get_title($code, $item_no);
+					$result = "$sku\t$title" . PHP_EOL;
+				} else {
+					$result = "$sku\tInvalid" . PHP_EOL;
+				}
+				fwrite($output, $result);
+			}
+		}
+	}
+
+	$status->log_status("Done!");
+	fclose($input);
+	fclose($output);
+}
+?>
