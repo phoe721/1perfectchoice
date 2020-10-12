d37f55591409eb0248ec0072d1a53d2f41e7b2b0
Aaron Lin <phoe721@gmail.com>
2020-10-12 13:57:36 +0800

    Update

--- script/ftpRenameFiles.php
+++ script/ftpRenameFiles.php
@@ -1,26 +1,25 @@
 <?
 require_once("class/ftp_client.php");
 $ftp_client = new ftp_client();
+$total = $renameCount = $testrun = 0;
+$testrun = 1;
+if (isset($argv[1])) {
+	// Connect to server
+	$ftp_client->connect(FTP_SERVER);
+	$ftp_client->login(FTP_USER, FTP_PASS);
+	$ftp_client->set_passive();
 
-// Connect to server
-$ftp_client->connect(FTP_SERVER);
-$ftp_client->login(FTP_USER, FTP_PASS);
-$ftp_client->set_passive();
-
-$total = $renameCount = 0;
-if (isset($argv[1]) && isset($argv[2])) {
 	$path = $argv[1];
-	$test = $argv[2]; // 0 - Actual Run, 1 - Test Run
 	$files = $ftp_client->list_files($path);
 	$total = count($files);
 	$ftp_client->change_dir($path);
 	foreach($files as $file) {
 		if(preg_match('/-[0-9].jpg/', $file)) {
 			// Convert -2, -3 to _2, -3 etc. -1 will be removed.
-			$new_file = preg_replace('/([0-9]+)-([0-9].jpg)/','\1_\2', $file);
+			$new_file = preg_replace('/([0-9A-Z]+)-([0-9].jpg)/','\1_\2', $file);
 			$new_file = preg_replace('/(_1)(.jpg)/','\2', $new_file);
 			printf("$file is going to rename to $new_file!\n");
-			if(!$test && $ftp_client->rename($file, $new_file)) {
+			if(!$testrun && $ftp_client->rename($file, $new_file)) {
 				printf("$file has been rename to $new_file!\n");
 				$renameCount++;
 			} else {
@@ -34,7 +33,7 @@ if (isset($argv[1]) && isset($argv[2])) {
 			//Convert JPG to jpg
 			$new_file = preg_replace('/JPG/','jpg', $file); 
 			printf("$file is going to rename to $new_file!\n");
-			if(!$test && $ftp_client->rename($file, $new_file)) {
+			if(!$testrun && $ftp_client->rename($file, $new_file)) {
 				printf("$file has been rename to $new_file!\n");
 				$renameCount++;
 			} else {
@@ -46,6 +45,8 @@ if (isset($argv[1]) && isset($argv[2])) {
 	}
 	
 	printf("Total: %d Renamed: %d\n", $total, $renameCount);
+} else {
+	printf("Usage: php ftpRenameFiles.php path\n");
 }
 
 // Disconnect from server
