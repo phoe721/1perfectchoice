0c7a2f06f39f79e9e0a06236b7e15f2747fe0aaf
Aaron Lin <phoe721@gmail.com>
2021-03-06 10:12:02 +0800

    Update

--- script/ftpBackupFiles.php
+++ script/ftpBackupFiles.php
@@ -13,10 +13,10 @@ foreach($file_list as $file) {
 	$ftp_url = "ftp://" . FTP_USER . ":" . FTP_PASS . "@" . FTP_SERVER . $remote_dir . $file;
 	//printf("FTP URL: $ftp_url\n");
 	if ($file == '.' || $file == '..' || $file == 'sofa-360' || $file == 'Rumor-Center-USA' || $file == 'Others' || $file == 'Furniture-Picture' || $file == 'ebay' || $file == 'ADJA' || $file == '20120617' || $file == '20120528') {
-		printf("Skipping directory - $file\n");
+		//printf("Skipping directory - $file\n");
 		continue;
 	} else if (pathinfo($file, PATHINFO_EXTENSION)) {
-		printf("Skipping file - $file\n");
+		//printf("Skipping file - $file\n");
 		continue;
 	} else if (is_dir($ftp_url)) {
 		$remote_subdir = $remote_dir . $file;
@@ -40,7 +40,7 @@ foreach($file_list as $file) {
 			$remote_path = $remote_subdir . "/" . $file2;
 			$local_path = $local_subdir . "/" . $file2;
 			if (file_exists($local_path)) {
-				printf("File exists - $file2\n");
+				//printf("File exists - $file2\n");
 			} else {
 				printf("Downloading $local_path...");
 				if ($ftp_client->get($local_path, $file2)) {
