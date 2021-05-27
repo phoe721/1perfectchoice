4f7eee654ace848ccf8ea15a40c2360ca87864a4
Aaron Lin <phoe721@gmail.com>
2021-05-27 20:43:00 +0800

    Update

--- script/ftpBackupFiles.php
+++ script/ftpBackupFiles.php
@@ -8,50 +8,63 @@ $local_dir = "/home/aaron/images/";
 $ftp_client->connect(FTP_SERVER);
 $ftp_client->login(FTP_USER, FTP_PASS);
 $ftp_client->set_passive();
+$ftp_client->change_dir($remote_dir);
 $file_list = $ftp_client->get_list_files($remote_dir);
 foreach($file_list as $file) {
-	$ftp_url = "ftp://" . FTP_USER . ":" . FTP_PASS . "@" . FTP_SERVER . $remote_dir . $file;
+	$ftp_url = "ftp://" . FTP_USER . ":" . FTP_PASS . "@" . FTP_SERVER . $file;
 	//printf("FTP URL: $ftp_url\n");
-	if ($file == '.' || $file == '..' || $file == 'sofa-360' || $file == 'Rumor-Center-USA' || $file == 'Others' || $file == 'Furniture-Picture' || $file == 'ebay' || $file == 'ADJA' || $file == '20120617' || $file == '20120528') {
-		//printf("Skipping this directory - $file\n");
-		continue;
-	} else if (pathinfo($file, PATHINFO_EXTENSION)) {
+	if (pathinfo($file, PATHINFO_EXTENSION)) {
 		//printf("Skipping file - $file\n");
 		continue;
-	} else if (is_dir($ftp_url) && $file == "PDEX") {
-		$remote_subdir = $remote_dir . $file;
-		$local_subdir = $local_dir . $file;
-		if (is_dir($local_subdir)) {
-			printf("Directory exists - $local_subdir\n");
-			// Do Nothing
+	} else if (is_dir($ftp_url)) {
+		$filename = basename($file);
+		if ($filename == '.' || $filename == '..' || $filename == 'sofa-360' || $filename == 'Rumor-Center-USA' || $filename == 'Others' || $filename == 'Furniture-Picture' || $filename == 'ebay' || $filename == 'ADJA' || $filename == '20120617' || $filename == '20120528') {
+			printf("Skipping this directory - $filename\n");
+			continue;
 		} else {
-			if (mkdir($local_subdir, 0755)) {
-				printf("Directory created - $local_subdir\n");
+			$local_subdir = $local_dir . $filename;
+			if (is_dir($local_subdir)) {
+				printf("Directory exists - $local_subdir\n");
+				// Do Nothing
 			} else {
-				printf("Failed to create - $local_subdir\n");
-				continue;
-			}
-		}
-
-		$ftp_client->change_dir($remote_subdir);
-		$file_list2 = $ftp_client->get_list_files($remote_subdir);
-		foreach($file_list2 as $file2) {
-			if ($file2 == '.' || $file2 == '..') continue;
-			$remote_path = $remote_subdir . "/" . $file2;
-			$local_path = $local_subdir . "/" . $file2;
-			$remote_mtime = $ftp_client->mdtm($file2);
-			$local_mtime = filemtime($local_path);
-			if (file_exists($local_path) && ($local_mtime > $remote_mtime)) {
-				//printf("File exists - $file2\n");
-			} else {
-				printf("Downloading $local_path...");
-				if ($ftp_client->get($local_path, $file2)) {
-					printf("Downloaded\n");
+				if (mkdir($local_subdir, 0755)) {
+					printf("Directory created - $local_subdir\n");
 				} else {
-					printf("Failed to download\n");
+					printf("Failed to create - $local_subdir\n");
+					continue;
+				}
+			}
+
+			$ftp_client->change_dir($file);
+			$file_list2 = $ftp_client->get_list_files($file);
+			foreach($file_list2 as $file2) {
+				$filename2 = basename($file2);
+				if ($filename2 == '.' || $filename2 == '..') continue;
+				$local_path = $local_subdir . "/" . $filename2;
+				if (file_exists($local_path)) {
+					$remote_mtime = $ftp_client->mdtm($file2);
+					$local_mtime = filemtime($local_path);
+					if ($local_mtime > $remote_mtime) {
+						printf("File exists - $file2\n");
+					} else {
+						printf("Downloading $local_path...");
+						if ($ftp_client->get($local_path, $file2)) {
+							printf("Downloaded\n");
+						} else {
+							printf("Failed to download\n");
+						}
+					}
+				} else {
+					printf("Downloading $local_path...");
+					if ($ftp_client->get($local_path, $file2)) {
+						printf("Downloaded\n");
+					} else {
+						printf("Failed to download\n");
+					}
 				}
 			}
 		}
+
 	}
 }
 
