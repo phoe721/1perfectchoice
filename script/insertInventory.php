c5b5da8917e8dd9753da251a26d5127161a5a2e7
Aaron Lin <phoe721@gmail.com>
2022-04-23 20:00:39 +0800

    Update

--- script/insertInventory.php
+++ script/insertInventory.php
@@ -5,12 +5,14 @@ require_once("class/validator.php");
 $inventory = new inventory();
 $status = new status();
 $validator = new validator();
+$lines = $count = $inStock = 0;
 
 if (isset($argv[1]) && isset($argv[2]) && isset($argv[3])) {
 	$inputFile = $argv[1];
 	$outputFile = $argv[2];
 	$statusFile = $argv[3];
 	$status->set_file($statusFile);
+	$lines = count(file($inputFile));
 	$input = fopen($inputFile, "r");
 	$output = fopen($outputFile, "a+");
 	if ($input && $output) {
@@ -24,6 +26,7 @@ if (isset($argv[1]) && isset($argv[2]) && isset($argv[3])) {
 					if ($inventory->check_exist($code, $item_no)) {
 						$result = "$sku\tExists". PHP_EOL;
 					} else {
+						if (!empty($qty) && $qty > 0) $count++;
 						$result = $inventory->insert($code, $item_no, $qty);
 						$result = $result ? "$sku\tOK" . PHP_EOL : "$sku\tFail" . PHP_EOL;
 					}
@@ -35,6 +38,8 @@ if (isset($argv[1]) && isset($argv[2]) && isset($argv[3])) {
 		}
 	}
 
+	$inStock = round(($count / $lines) * 100, 2);
+	fwrite($output, "There are total $lines SKU(s) and $count with quantity. The percentage is $inStock%.\n");
 	$status->log_status("Done!");
 	fclose($input);
 	fclose($output);
