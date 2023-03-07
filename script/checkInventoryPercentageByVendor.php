88fd6fbb2f81edd03374b5d23be0cb10d36b0958
Aaron Lin <phoe721@gmail.com>
2023-03-07 20:38:49 +0800

    Update

--- script/checkInventoryPercentageByVendor.php
+++ script/checkInventoryPercentageByVendor.php
@@ -1,9 +1,9 @@
 <?
 require_once("class/inventory.php");
-require_once("class/debugger.php");
 $inventory = new inventory();
 $output = new debugger();
 $output->set_console(true);
+$set_mail = false;
 
 $result = "";
 $vendors = $inventory->get_vendors();
@@ -13,7 +13,7 @@ foreach($vendors as $vendor) {
 	$inStockPercentage = 100 * round($inStock / $itemCount, 2);
 	$result .= "$vendor has total $itemCount items and $inStock items are in stock. In stock percentage is $inStockPercentage%" . PHP_EOL;
 }
-
 $output->info($result);
-mail(ADMIN_GROUP_MAIL,"Inventory Percentage",$result);
+
+if ($set_mail) mail(ADMIN_GROUP_MAIL,"Inventory Percentage",$result);
 ?>
