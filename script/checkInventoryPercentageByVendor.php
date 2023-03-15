bc78fe040b152c290ee247ccfffeb5233efeb2f7
Aaron Lin <phoe721@gmail.com>
2023-03-15 20:40:05 +0800

    Update

--- script/checkInventoryPercentageByVendor.php
+++ script/checkInventoryPercentageByVendor.php
@@ -7,11 +7,15 @@ $set_mail = false;
 
 $result = "";
 $vendors = $inventory->get_vendors();
-foreach($vendors as $vendor) {
-	$inStock = $inventory->check_vendor_in_stock_count($vendor);
-	$itemCount = $inventory->check_vendor_item_count($vendor);
-	$inStockPercentage = 100 * round($inStock / $itemCount, 2);
-	$result .= "$vendor has total $itemCount items and $inStock items are in stock. In stock percentage is $inStockPercentage%" . PHP_EOL;
+if (!empty($vendors)) {
+	foreach($vendors as $vendor) {
+		$inStock = $inventory->check_vendor_in_stock_count($vendor);
+		$itemCount = $inventory->check_vendor_item_count($vendor);
+		$inStockPercentage = 100 * round($inStock / $itemCount, 2);
+		$result .= "$vendor has total $itemCount items and $inStock items are in stock. In stock percentage is $inStockPercentage%" . PHP_EOL;
+	}
+} else {
+	$result = "Inventory is empty!";
 }
 $output->info($result);
 
